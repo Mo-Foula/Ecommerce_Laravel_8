0e323f7ba0ca4553ff90e139e493648b953f1d4a
martina mamdouh <martinadouha2000@gmail.com>
2021-09-20 00:20:11 +0200

    .

--- database/migrations/2021_09_15_231852_create_order_items_table.php
+++ database/migrations/2021_09_15_231852_create_order_items_table.php
@@ -21,7 +21,7 @@ class CreateOrderItemsTable extends Migration
             $table->integer('quantity');      
             $table->timestamps();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
 
 
         });
