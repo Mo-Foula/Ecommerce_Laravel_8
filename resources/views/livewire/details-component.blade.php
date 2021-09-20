bdefeefd6b0751fb10e11871ee14ca3aba39e746
martina mamdouh <martinadouha2000@gmail.com>
2021-09-20 17:32:08 +0200

    delete from cart when place order

--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -38,7 +38,7 @@
                     <div class="stock-info in-stock">
                         <p class="availability">Availability: <b>{{$product->stock_status}}</b></p>
                     </div>
-                    <div class="quantity">
+                    <!-- <div class="quantity">
                         <span>Quantity:</span>
                         <div class="quantity-input">
                             <input type="text" name="product-quatity" value="1" data-max="120" pattern="[0-9]*" >
@@ -46,7 +46,7 @@
                             <a class="btn btn-reduce" href="#"></a>
                             <a class="btn btn-increase" href="#"></a>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="wrap-butons">
                         <a href="#" class="btn add-to-cart" wire:click.prevent="store({{$product->id}},'{{$product->name}}',{{$product->regular_price}})">Add to Cart</a>
                     </div>
